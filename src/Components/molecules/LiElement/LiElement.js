516dc74abcc9204105236b6de792248f557d2c6b
Bertha <palm.sins@gmail.com>
2022-06-03 00:02:52 +0200

    added closeMenu function to li elements

--- src/Components/molecules/LiElement/LiElement.js
+++ src/Components/molecules/LiElement/LiElement.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Anchor from "../../atoms/Anchor/Anchor";
 
-export default function LiElement({ data }) {
+export default function LiElement({ data, funcMenuClose, customClass }) {
   const { liclass, url, name } = data;
   return (
-    <li className={liclass && liclass}>
+    <li
+      className={(liclass && liclass) || customClass}
+      onClick={() => funcMenuClose()}
+    >
       <Anchor url={`/${url ? url : name.toLowerCase()}`} content={name} />
     </li>
   );
