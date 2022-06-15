6bae96a62ddd291216667d7f7db75b586bc4b7e0
Bertha <palm.sins@gmail.com>
2022-06-16 00:39:41 +0200

    add styles and responsive behavior to random myth

--- src/Components/molecules/Cards/CardMyth.js
+++ src/Components/molecules/Cards/CardMyth.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { AiOutlineCloseCircle as CloseIcon } from "react-icons/ai";
 import { baseURL } from "../../../utils/axios";
 import Title from "../../atoms/Title/Title";
@@ -6,31 +7,33 @@ import Paragraph from "../../atoms/Paragraph/Paragraph";
 import FavHeart from "../../atoms/FavHeart/FavHeart";
 
 export default function CardMyth({ modal, data, funcClose }) {
+  const { loading } = useSelector((state) => state.myth);
+  console.log(loading);
   return (
     <div className="Block Detail-Block">
       {modal && (
         <CloseIcon className="Detail-Modal-Close" onClick={() => funcClose()} />
       )}
-      <Title
-        type="h3"
-        tClass="Title Title--small Detail-Block-Title"
-        tData={`Mythe`}
-      />
       <div className="Detail-Block-Container">
         <figure className="Detail-Picture">
+          <Title
+            type="h3"
+            tClass="Title Title--small Detail-Block-Title"
+            tData={data.constellation.latin_name}
+          />
           <img
             src={`${baseURL}${data.img_url || data.constellation.img_url}`}
             alt={data.constellation.name}
           />
-          <figcaption className="Title Title--small Detail-Picture-Title">
+          {/* <figcaption className="Title Title--small Detail-Picture-Title">
             {data.constellation.latin_name}
-          </figcaption>
+          </figcaption> */}
         </figure>
         <div className="Detail-Description">
           <Title
             type="h2"
             tClass="Detail-Description-Title"
-            tData={`Constellation ${data.constellation.name}`}
+            tData={`Mythe ${data.constellation.name}`}
           />
           <Paragraph
             cClass="Detail-Description-Text"
