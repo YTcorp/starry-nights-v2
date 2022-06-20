3a0daa50a737016ccf867b8d5474dd3b3f25fa0a
Bertha Torres <btorres@europe.altair.com>
2022-06-20 21:20:48 +0200

    handled code on data modal factorised

--- src/Components/molecules/ConstellationsList/ConstellationsList.js
+++ src/Components/molecules/ConstellationsList/ConstellationsList.js
@@ -1,52 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../../atoms/Spinner/Spinner";
 import LiElement from "../LiElement/LiElement";
 import { baseURL } from "../../../utils/axios";
-import HandleModalConstellation from "../../../utils/useHandleModalContent";
-import { send } from "process";
-// import { setModalContent } from "../../../store/features/modalSlice";
 
 export default function ConstellationsList() {
-  //   const dispatch = useDispatch();
   const { constellations, loading } = useSelector(
     (state) => state.constellation
   );
   const { favLoading } = useSelector((state) => state.userData);
-  const [sendDataModal, setSendDataModal] = useState(false);
-  const [oneConstellation, setOneConstellation] = useState("");
-  useEffect(() => {
-    if (sendDataModal) {
-      return <HandleModalConstellation data={oneConstellation} />;
-    }
-  }, [oneConstellation, sendDataModal]);
-
-  const handleDataModal = (constellation) => {
-    setSendDataModal(!sendDataModal);
-    setOneConstellation(constellation);
-    console.log(sendDataModal);
-    // if (sendDataModal) {
-    //   return (
-    //     <HandleModalConstellation
-    //       data={data}
-    //       key={`Constellations-List-SubItem--${index}`}
-    //     />
-    //   );
-    // }
-  };
-  console.log(sendDataModal);
-
-  //   useHandleModalConstellation(data);
-  //     const handleModal =(data) =>{
-  //     }
-
-  //   const handleModalConstellation = (data) => {
-  //     const isFav = favConstellations.find((favorite) => favorite.id === data.id)
-  //       ? true
-  //       : false;
-  //     const foundConstellation = { ...data, favorite: isFav };
-  //     dispatch(setModalContent(foundConstellation));
-  //   };
 
   return (
     <ul className="Constellations-List">
@@ -56,7 +18,6 @@ export default function ConstellationsList() {
           key={`Constellations-List-Item--${index}`}
           data={constellation}
           customClass="Constellations-List-Item"
-          funcMenu={() => handleDataModal(constellation)}
         >
           <img
             className="Constellations-List-Item-Image"
--- src/Components/molecules/LiElement/LiElement.js
+++ src/Components/molecules/LiElement/LiElement.js
@@ -1,38 +1,43 @@
 import React from "react";
-import Anchor from "../../atoms/Anchor/Anchor";
+import { useSelector, useDispatch } from "react-redux";
+import { setModalContent } from "../../../store/features/modalSlice";
+import Anchor from "../..//atoms/Anchor/Anchor";
 
 export default function LiElement(props) {
-  if (typeof props.data.url !== "undefined") {
-    return (
-      <li
-        className={
-          (props.data.liclass && props.data.liclass) || props.customClass
-        }
-        onClick={() => props.funcMenu()}
-      >
-        <Anchor
-          url={`/${
-            props.data.url ? props.data.url : props.data.name.toLowerCase()
-          }`}
+  const { favConstellations } = useSelector((state) => state.userData);
+  const dispatch = useDispatch();
+
+  const handleData = (constellation) => {
+    const isFav = favConstellations.find((favorite) => favorite.id === constellation.id)
+      ? true
+      : false;
+    const foundConstellation = { ...constellation, favorite: isFav };
+    dispatch(setModalContent(foundConstellation));
+  }
+
+  return (
+    <li
+      className={
+        (props.data.liclass && props.data.liclass) || props.customClass
+      }
+      onClick={() => {
+        handleData && handleData(props.data);
+        props.funcMenu && props.funcMenu();
+      }
+      }
+    >
+      {typeof props.data.url !== "undefined"
+        ? <Anchor
+          url={`/${props.data.url ? props.data.url : props.data.name.toLowerCase()}`}
           content={props.data.name}
         />
-      </li>
-    );
-  } else {
-    console.log("noURL >>>>>>>>>>>>>>>>>>>>>>>>>>", props.funcMenu);
-    return (
-      <li
-        className={
-          (props.data.liclass && props.data.liclass) || props.customClass
-        }
-        onClick={props.funcMenu && (() => props.funcMenu())}
-      >
-        {props.children
+        :
+        props.children
           ? props.children.length > 1
             ? props.children.map((element) => element)
             : props.children
-          : props.data.name}
-      </li>
-    );
-  }
+          : props.data.name
+      }
+    </li>
+  )
 }
