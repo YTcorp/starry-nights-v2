1a6e638e70c1b63babb3b48f56d4480ac53633d6
Bertha <palm.sins@gmail.com>
2022-06-26 19:57:11 +0200

    added a instance with interceptor for axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,21 @@
 import axios from "axios";
+import { setupInterceptorsTo } from "../helpers/interceptors";
 
 export const baseURL = "http://54.38.188.38:5000";
 export const ApiBaseURL = `${baseURL}/v1/api`;
 
-export default axios.create({
+export const api_restricted = setupInterceptorsTo(
+  axios.create({
+    baseURL: ApiBaseURL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+);
+
+export const api = axios.create({
   baseURL: ApiBaseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
