ec2c805e63fda4d6146c57207fa2ec392e460f7d
Bertha <palm.sins@gmail.com>
2022-07-02 15:42:19 +0200

    fixed render on empty fav constellations

--- src/Components/templates/MyConstellations.js
+++ src/Components/templates/MyConstellations.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BiTrash } from "react-icons/bi";
+import { isEmpty } from "lodash";
 
 import LiElement from "../molecules/LiElement/LiElement";
 import Spinner from "../atoms/Spinner/Spinner";
@@ -24,30 +25,38 @@ export default function MyConstellations() {
     <main className="Main MyConstellations">
       <h1 className="Title Page-Title">Mes Constellations</h1>
       <div className="Block myconstellation">
-        <div className="myconstellation-container">
-          <h2 className="myconstellation-title">Ouvrir</h2>
-          <h2 className="myconstellation-title">Effacer</h2>
-          {favLoading && <Spinner />}
-          {favConstellations.map((favConstellation) => {
-            return (
-              <div
-                className="myconstellation-inner-container"
-                key={`key-${favConstellation.id}`}
-              >
-                <LiElement
-                  key={`myconstellation-${favConstellation.id}`}
-                  customClass="myconstellation-element"
-                  data={favConstellation}
-                />
-                <BiTrash
-                  key={`myconstellation-icon-${favConstellation.id}`}
-                  className="myconstellation-icon"
-                  onClick={() => deleteFavConstellation(favConstellation.id)}
-                />
-              </div>
-            );
-          })}
-        </div>
+        {isEmpty(favConstellations) ? (
+          <h2 className="Subtitle">Pas de constellations encore !</h2>
+        ) : (
+          <>
+            <div className="myconstellation-container">
+              <h2 className="myconstellation-title">Ouvrir</h2>
+              <h2 className="myconstellation-title">Effacer</h2>
+              {favLoading && <Spinner />}
+              {favConstellations.map((favConstellation) => {
+                return (
+                  <div
+                    className="myconstellation-inner-container"
+                    key={`key-${favConstellation.id}`}
+                  >
+                    <LiElement
+                      key={`myconstellation-${favConstellation.id}`}
+                      customClass="myconstellation-element"
+                      data={favConstellation}
+                    />
+                    <BiTrash
+                      key={`myconstellation-icon-${favConstellation.id}`}
+                      className="myconstellation-icon"
+                      onClick={() =>
+                        deleteFavConstellation(favConstellation.id)
+                      }
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
