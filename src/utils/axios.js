7c1e3c8fcbc761842983e6f4c2c2bfbfc1fb2de0
Bertha <palm.sins@gmail.com>
2022-08-13 15:51:00 +0200

    https

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { setupInterceptorsTo } from "../helpers/interceptors";
 
 // export const baseURL = "http://localhost:3001";
-export const baseURL = "http://starrynights.tato-api.ovh";
+export const baseURL = "https://starrynights.tato-api.ovh";
 export const ApiBaseURL = `${baseURL}/v1/api`;
 
 // export const api_restricted = setupInterceptorsTo(
